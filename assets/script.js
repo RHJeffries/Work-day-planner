e3b243d9388c74c843eff71d7c1f600c7b902366
RHJeffries <88922391+RHJeffries@users.noreply.github.com>
2021-10-09 17:41:57 +1030

    colour coding by time functional

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,49 @@
+//display current day
 var today = moment();
-$("#currentDay").text(today.format("dddd, MMMM Do"));
+$("#currentDay").text(today.format("dddd, Do MMMM"));
+
+var timeNow = moment();
+console.log(timeNow);
+var hour = parseInt(timeNow.format("H"));
+console.log(hour);
+
+
+
+
+//$(".time-block").addClass("present");
+
+function colorTime() {
+$(".time-block").each(function () {
+  var plannedTime = parseInt($(this).attr("id"));
+  if (plannedTime > hour){
+   $(this).addClass("future")
+  } 
+  else if (plannedTime === hour) {
+    $(this).addClass("present");
+  }
+  else  {
+    $(this).addClass("past");
+  }
+})
+}
  
+colorTime();
+    /*$(".saveBtn").click(function () {
+        
+        // val() method is used to get the values from 
+       // textarea and stored in txt variable
+        var txt = $("#textarea").val();
+        localStorage.setItem("txt", );
+    });*/
+
+   // saveBtn.addEventListener("click", function(event){
+     //   event.preventDefault();
+
+   /*  $('.saveBtn').on("click", function() {
+       console.log("clicked");
+       var  = 
+       $(this).attr('id');
+       $("#input" + btnNum).val();
+     });*/
+
+
